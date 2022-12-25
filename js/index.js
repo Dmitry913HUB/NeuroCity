db721e1dab566de7428093394308f512f05d91c1
Dmitry913HUB <58403614+Dmitry913HUB@users.noreply.github.com>
2022-12-25 21:17:37 +0300

    complite 7,8

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.body.onload = function(){
+    setTimeout(function(){
+        var preloder = document.getElementById('page-preloder');
+        if(!preloder.classList.contains('done')){
+            preloder.classList.add('done');
+        }
+    }, 2000)
+}
